d7e901a03dff6c1543d6e44178b9dd27eb43242a
Mark McDermott <mcdermottsolutions@gmail.com>
2015-12-19 06:00:23 -0600

    small copy and icon changes

--- www/js/services.js
+++ www/js/services.js
@@ -7,27 +7,27 @@ angular.module('starter.services', [])
   var chats = [{
     id: 0,
     name: 'Ben Sparrow',
-    lastText: 'You on your way?',
+    lastText: 'Did you get the pizza yet?',
     face: 'img/ben.png'
   }, {
     id: 1,
     name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
+    lastText: 'Yeah, but I got anchovies and pineapple.',
     face: 'img/max.png'
   }, {
     id: 2,
     name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
+    lastText: 'I\'d rather eat a shoe.',
     face: 'img/adam.jpg'
   }, {
     id: 3,
     name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
+    lastText: 'Very nutritious.',
     face: 'img/perry.png'
   }, {
     id: 4,
     name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
+    lastText: 'This is wicked good pizza.',
     face: 'img/mike.png'
   }];
 
